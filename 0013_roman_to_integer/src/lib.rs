3e94c3b27e5782a9d425c4ab47cfccd1ed55b262
heeki <heekipark@gmail.com>
2024-10-03 15:14:21 -0400

    refactor(0013_roman_to_integer): simplified logic

--- 0013_roman_to_integer/src/lib.rs
+++ 0013_roman_to_integer/src/lib.rs
@@ -2,6 +2,15 @@ use std::collections::HashMap;
 
 pub struct Solution {}
 impl Solution {
+    fn is_substraction_case(current: char, next: Option<char>) -> bool {
+        match (current, next) {
+            ('I', Some('V' | 'X')) => true,
+            ('X', Some('L' | 'C')) => true,
+            ('C', Some('D' | 'M')) => true,
+            _ => false,
+        }
+    }
+
     pub fn roman_to_int(x: String) -> i32 {
         let map: HashMap<char, i32> = HashMap::from([
             ('I', 1),
@@ -13,27 +22,12 @@ impl Solution {
             ('M', 1000)
         ]);
         let mut result = 0;
-        for (i,c) in x.chars().enumerate() {
-            if x.chars().nth(i).unwrap() == 'I' {
-                if i+1 < x.chars().count() && (x.chars().nth(i+1).unwrap() == 'V' || x.chars().nth(i+1).unwrap() == 'X') {
-                    result -= map.get(&c).copied().unwrap_or(0);
-                } else {
-                    result += map.get(&c).copied().unwrap_or(0);
-                }
-            } else if x.chars().nth(i).unwrap() == 'X' {
-                if i+1 < x.chars().count() && (x.chars().nth(i+1).unwrap() == 'L' || x.chars().nth(i+1).unwrap() == 'C') {
-                    result -= map.get(&c).copied().unwrap_or(0);
-                } else {
-                    result += map.get(&c).copied().unwrap_or(0);
-                }
-            } else if x.chars().nth(i).unwrap() == 'C' {
-                if i+1 < x.chars().count() && (x.chars().nth(i+1).unwrap() == 'D' || x.chars().nth(i+1).unwrap() == 'M') {
-                    result -= map.get(&c).copied().unwrap_or(0);
-                } else {
-                    result += map.get(&c).copied().unwrap_or(0);
-                }
+        for (i, c) in x.chars().enumerate() {
+            let value = map.get(&c).copied().unwrap_or(0);
+            if Solution::is_substraction_case(c, x.chars().nth(i+1)) {
+                result -= value;
             } else {
-                result += map.get(&c).copied().unwrap_or(0);
+                result += value;
             }
         }
         result
